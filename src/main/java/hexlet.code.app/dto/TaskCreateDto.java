de166a3b498eca82911c7318aec119388c86e5c2
EgorAL15 <alexanderovesar56789@gmail.com>
2026-09-23 09:24:16 +0300

    Support both labels and taskLabelIds on task creation

--- src/main/java/hexlet.code.app/dto/TaskCreateDto.java
+++ src/main/java/hexlet.code.app/dto/TaskCreateDto.java
@@ -1,5 +1,6 @@
 package hexlet.code.app.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 
@@ -23,7 +24,8 @@ public class TaskCreateDto {
     @JsonProperty("assignee_id")
     private Long assigneeId;
 
-    private Set<Long> taskLabelIds;
+    @JsonAlias("taskLabelIds")
+    private Set<Long> labels;
 
     public String getName() {
         return name;
@@ -45,8 +47,8 @@ public class TaskCreateDto {
         return assigneeId;
     }
 
-    public Set<Long> getTaskLabelIds() {
-        return taskLabelIds;
+    public Set<Long> getLabels() {
+        return labels;
     }
 
     public void setName(String name) {
@@ -69,7 +71,7 @@ public class TaskCreateDto {
         this.assigneeId = assigneeId;
     }
 
-    public void setTaskLabelIds(Set<Long> taskLabelIds) {
-        this.taskLabelIds = taskLabelIds;
+    public void setLabels(Set<Long> labels) {
+        this.labels = labels;
     }
 }
