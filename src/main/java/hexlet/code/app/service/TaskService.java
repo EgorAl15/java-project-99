c62cabe037c6de0f37c19e6b03133e117a327195
EgorAL15 <alexanderovesar56789@gmail.com>
2026-09-23 09:14:36 +0300

    Map taskLabelIds to task labels

--- src/main/java/hexlet/code/app/service/TaskService.java
+++ src/main/java/hexlet/code/app/service/TaskService.java
@@ -88,8 +88,8 @@ public class TaskService {
             task.setAssignee(assignee);
         }
 
-        if (dto.getLabels() != null) {
-            task.setLabels(findLabels(dto.getLabels()));
+        if (dto.getTaskLabelIds() != null) {
+            task.setLabels(findLabels(dto.getTaskLabelIds()));
         }
 
         return toDto(taskRepository.save(task));
@@ -131,8 +131,8 @@ public class TaskService {
             task.setAssignee(assignee);
         }
 
-        if (dto.getLabels() != null) {
-            task.setLabels(findLabels(dto.getLabels()));
+        if (dto.getTaskLabelIds() != null) {
+            task.setLabels(findLabels(dto.getTaskLabelIds()));
         }
 
         return toDto(taskRepository.save(task));
